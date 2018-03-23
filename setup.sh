bf89d268c10658271998fbb7b0e64a0771308977
js-jslog <josephsinfield@yahoo.com>
2018-03-23 00:31:59 +0000

    setup script now persists original files if they exist so that no information is lost. provision of the bash.d folder also improved

--- setup.sh
+++ setup.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-# remove existing links (this should probably just move original files if they are files)
-rm ~/.bashrc || true
-rm ~/.bash_aliases || true
-rm ~/.vimrc || true
-rm ~/.tmux.conf || true
+# if any original files exist then we will just move them rather than 
+# delete them
+# if the file is a symlink then it will have in all likelyhood been 
+# provisioned by a version controlled dotfile manager so can just
+# be overwritten by stage 2 below
+if [[ ! -L ~/.bashrc ]]; then
+  mv ~/.bashrc ~/.bashrc.original 2>/dev/null
+fi
+if [[ ! -L ~/.bash_aliases ]]; then
+  mv ~/.bash_aliases ~/.bash_aliases.original 2>/dev/null
+fi
+if [[ ! -L ~/.vimrc ]]; then
+  mv ~/.vimrc ~/.vimrc.original 2>/dev/null
+fi
+if [[ ! -L ~/.tmux.conf ]]; then
+  mv ~/.tmux.conf ~/.tmux.conf.original 2>/dev/null
+fi
 
-# link file system locations to dotfiles-root issuers files
+# overwrite existing symbolic links if they exist
 ln -sf ~/dotfiles-root/.bashrc ~/.bashrc
 ln -sf ~/dotfiles-root/.bash_aliases ~/.bash_aliases
-rm -r ~/dotfiles-root/bash.d/ || true
-mkdir ~/dotfiles-root/bash.d/
-ln -sf ~/dotfiles-root/bash.d ~/bash.d
 ln -sf ~/dotfiles-root/.vimrc ~/.vimrc
 ln -sf ~/dotfiles-root/.tmux.conf ~/.tmux.conf
+# overwriting sybolic links doesn't work if they are linked to directories apparently
+# need to remove it
+rm ~/bash.d || true
+# the bash.d directory doesn't have any of it's own files, so it is excluded from the repo and
+# we need to create it during runtime
+rm -r ~/dotfiles-root/bash.d/ || true
+mkdir ~/dotfiles-root/bash.d/
+# now we can create the link
+ln -s ~/dotfiles-root/bash.d ~/bash.d
 
+# filling the bash.d folder with scripts to be run at shell initiation
 # bash files should contain exported environment variables and functions for interactive shells
 if [ -d ~/dotfiles-rullion/bash.d/ ]; then
   for bashpath in $(find ~/dotfiles-rullion/bash.d/ \( -name '*.sh' \))
@@ -31,6 +50,7 @@ if [ -d ~/dotfiles-personal/bash.d/ ]; then
   done
 fi
 
+# filling the profile.d folder with scripts to be run at login shell initiation
 # profile files should contain exported environment variables and functions for login shells
 if [ -d ~/dotfiles-rullion/profile.d/ ]; then
   for profilepath in $(find ~/dotfiles-rullion/profile.d/ \( -name '*.sh' \))
