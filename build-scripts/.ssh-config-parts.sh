0bc8502ef65cf89723612f6e71a3d07e0f2ecddb
js-jslog <jhs4jbs@hotmail.co.uk>
2018-03-25 00:18:16 +0000

    added a check that the named dotfile repos are available at initial setup and removed [[ conditionals for compatibilitiy

--- build-scripts/.ssh-config-parts.sh
+++ build-scripts/.ssh-config-parts.sh
@@ -10,7 +10,7 @@ fi
 for dotfolder_name in $dotfolder_names; do
   # there is no composition inclusion mechanism for ssh (possibly for security)
   # so we need to build an actual file
-  if [[ -f ~/$dotfolder_name/.ssh/config ]]; then
+  if [ -f ~/$dotfolder_name/.ssh/config ]; then
     echo "# originally included from $dotfolder_name" >> $multiplexer/built-dots/.ssh/config
     cat ~/$dotfolder_name/.ssh/config >> $multiplexer/built-dots/.ssh/config
     printf "# end of file\n\n" >> $multiplexer/built-dots/.ssh/config
--- setup.sh
+++ setup.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
+set -e
+
 multiplexer="$PWD/${0%/*}"
 
 # if no dotfile-multiplex config file exists, copy the template file
-if [[ ! -f ~/dotfiles-multiplexer.conf ]]; then
+if [ ! -f ~/dotfiles-multiplexer.conf ]; then
   cp $multiplexer/dotfiles-multiplexer.conf.template ~/dotfiles-multiplexer.conf
 fi
 
 # load in the user configured include vars
 . ~/dotfiles-multiplexer.conf
 
+# check whether a working setup has been configured
+. $multiplexer/build-scripts/check-setup.sh
+
 # if any original files exist then we will just move them rather than 
 # delete them
 # if the file is a symlink then it will have in all likelyhood been 
 # provisioned by a version controlled dotfile manager so can just
 # be overwritten by stage 3 below
-if [[ ! -L ~/.bashrc ]]; then
+if [ ! -L ~/.bashrc ]; then
   mv ~/.bashrc ~/.bashrc.original 2>/dev/null
 fi
-if [[ ! -L ~/.bash_aliases ]]; then
+if [ ! -L ~/.bash_aliases ]; then
   mv ~/.bash_aliases ~/.bash_aliases.original 2>/dev/null
 fi
-if [[ ! -L ~/.vimrc ]]; then
+if [ ! -L ~/.vimrc ]; then
   mv ~/.vimrc ~/.vimrc.original 2>/dev/null
 fi
-if [[ ! -L ~/.tmux.conf ]]; then
+if [ ! -L ~/.tmux.conf ]; then
   mv ~/.tmux.conf ~/.tmux.conf.original 2>/dev/null
 fi
-if [[ ! -L ~/.gitconfig ]]; then
+if [ ! -L ~/.gitconfig ]; then
   mv ~/.gitconfig ~/.gitconfig.original 2>/dev/null
 fi
-if [[ ! -L ~/.ssh/config ]]; then
+if [ ! -L ~/.ssh/config ]; then
   # we need to provision the .ssh folder, just in case it doesn't exist yet
   mkdir -p ~/.ssh
   mv ~/.ssh/config ~/.ssh/config.original 2>/dev/null
@@ -43,13 +48,13 @@ rm ~/bash.d 2>/dev/null
 rm -r $multiplexer/built-dots/ 2>/dev/null
 mkdir -p $multiplexer/built-dots/.ssh
 mkdir -p $multiplexer/built-dots/bash.d
-. $multiplexer/build-scripts/.bash_aliases-includes.sh $include_aliases
-. $multiplexer/build-scripts/.vimrc-includes.sh $include_vimrc
-. $multiplexer/build-scripts/.gitconfig-includes.sh $include_gitconfig
-. $multiplexer/build-scripts/.tmux.conf-includes.sh $include_tmuxconf
-. $multiplexer/build-scripts/.ssh-config-parts.sh $include_sshconfig
-. $multiplexer/build-scripts/bash.d-symlinks.sh $include_bashd
-. $multiplexer/build-scripts/profile.d-symlinks.sh $include_profiled
+. $multiplexer/build-scripts/.bash_aliases-includes.sh ${config[include_aliases]}
+. $multiplexer/build-scripts/.vimrc-includes.sh ${config[include_vimrc]}
+. $multiplexer/build-scripts/.gitconfig-includes.sh ${config[include_gitconfig]}
+. $multiplexer/build-scripts/.tmux.conf-includes.sh ${config[include_tmuxconf]}
+. $multiplexer/build-scripts/.ssh-config-parts.sh ${config[include_sshconfig]}
+. $multiplexer/build-scripts/bash.d-symlinks.sh ${config[include_bashd]}
+. $multiplexer/build-scripts/profile.d-symlinks.sh ${config[include_profiled]}
 
 # overwrite existing symbolic links if they exist
 ln -sf $multiplexer/.bashrc ~/.bashrc
