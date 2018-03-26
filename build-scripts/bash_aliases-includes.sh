4417a8d8bb8f4dc0be80c7a8241eea1416b6fb91
js-jslog <jhs4jbs@hotmail.co.uk>
2018-03-26 20:03:32 +0100

    improving the location variable

--- build-scripts/bash_aliases-includes.sh
+++ build-scripts/bash_aliases-includes.sh
@@ -7,6 +7,11 @@ if [ ! $multiplexer ]; then
   exit 1
 fi
 
+# add some aliases for interacting with the dotfiles-multiplexer
+echo "export dotfiles_multiplexer=$multiplexer" >> $multiplexer/built-dots/.bash_aliases
+echo "alias dotsmplex=\"cd \$dotfiles_multiplexer\"" >> $multiplexer/built-dots/.bash_aliases
+
+
 for dotfolder_name in $dotfolder_names; do
   # no longer checking for the presence of these files in the build process as we want the user to 
   # have the flexibility to remove the files if they want and for the .bash_aliases file to continue
