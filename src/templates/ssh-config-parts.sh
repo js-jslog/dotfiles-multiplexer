fedf14b86d59e06381d730f970b4fb30cac16b3e
js-jslog <jhs4jbs@hotmail.co.uk>
2018-04-13 00:04:44 +0100

    improving the ssh composition

--- src/templates/ssh-config-parts.sh
+++ src/templates/ssh-config-parts.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
-dotfolder_locations=$@
+# there is no composition inclusion mechanism for ssh (possibly for security)
+# so we need to build an actual file
+function buildSshconfig() {
+  local aliases=$@
+  local alias
+  for alias in $aliases; do
+    local dotsrepo=$(aliasesToLocations $alias)
+    if [ -f $dotsrepo/.ssh/config ]; then
+      echo "# originally included from $dotsrepo" >> $build/.ssh/config
+      cat $dotsrepo/.ssh/config >> $build/.ssh/config
+      printf "# end of file\n\n" >> $build/.ssh/config
+    fi
+  done
+}
 
 if [ ! $multiplexer ]; then
   echo "This script should only be run by the dotfile-multiplexer"
   exit 1
 fi
 
-for dotfolder_location in $dotfolder_locations; do
-  # there is no composition inclusion mechanism for ssh (possibly for security)
-  # so we need to build an actual file
-  if [ -f $dotfolder_location/.ssh/config ]; then
-    echo "# originally included from $dotfolder_location" >> $build/.ssh/config
-    cat $dotfolder_location/.ssh/config >> $build/.ssh/config
-    printf "# end of file\n\n" >> $build/.ssh/config
-  fi
-done
+buildSshconfig $@
