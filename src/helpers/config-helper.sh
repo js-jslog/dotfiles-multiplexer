04cd1480175e6c10e9424ed81db335e6045a306d
js-jslog <jhs4jbs@hotmail.co.uk>
2018-04-12 22:16:16 +0100

    combining and renaming config helper files

--- src/helpers/config-helper.sh
+++ src/helpers/config-helper.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+function aliasesToRepos() {
+  for alias in $@; do
+    local repo_var=setup_${alias}_repo
+    local repo=${!repo_var}
+    echo $repo
+  done
+}
+
+function aliasesToLocations() {
+  for alias in $@; do
+    echo "$multiplexer/build/repos/$alias/"
+  done
+}
+
 function filterExcludedAliases() {
   local aliases=$@
   local exclude_found=false
