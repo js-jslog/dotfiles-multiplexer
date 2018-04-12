286074a4906d33e40593101d1446dc405c7446ce
js-jslog <jhs4jbs@hotmail.co.uk>
2018-04-12 23:51:35 +0100

    improving tmux config composition

--- src/templates/tmux.conf-includes.sh
+++ src/templates/tmux.conf-includes.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
-dotfolder_locations=$@
+function composeTmuxconf() {
+  local aliases=$@
+  local alias
+  for alias in $aliases; do
+    local dotsrepo=$(aliasesToLocations $alias)
+    echo "if-shell \"[ -f $dotsrepo/.tmux.conf ]\" 'source $dotsrepo/.tmux.conf'" >> $build/.tmux.conf
+  done
+}
 
 if [ ! $multiplexer ]; then
   echo "This script should only be run by the dotfile-multiplexer"
   exit 1
 fi
 
-for dotfolder_location in $dotfolder_locations; do
-  echo "if-shell \"[ -f $dotfolder_location/.tmux.conf ]\" 'source $dotfolder_location/.tmux.conf'" >> $build/.tmux.conf
-done
+composeTmuxconf $@
