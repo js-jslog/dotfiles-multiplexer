b8dd0d89bc03479d42fc5f8e52d6782870bfb478
js-jslog <jhs4jbs@hotmail.co.uk>
2018-04-15 15:23:56 +0100

    adding a function to push all repo changes

--- src/dotfiles/bash.d/functions.sh
+++ src/dotfiles/bash.d/functions.sh
@@ -3,9 +3,19 @@
 function pulldots() {
   local cwd=$PWD
   for repo in $(ls $dotfiles_multiplexer/build/repos/); do
-    echo "PUlling $dotfiles_multiplexer/build/repos/$repo"
+    echo "Pulling $dotfiles_multiplexer/build/repos/$repo"
     cd $dotfiles_multiplexer/build/repos/$repo
     git pull
+  done
+  cd $cwd
+}
+
+function pushdots() {
+  local cwd=$PWD
+  for repo in $(ls $dotfiles_multiplexer/build/repos/); do
+    echo "Pushing $dotfiles_multiplexer/build/repos/$repo"
+    cd $dotfiles_multiplexer/build/repos/$repo
+    git push
   done
   cd $cwd
 }
