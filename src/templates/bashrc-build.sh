24ee54d1ae526fbf5affbc6cc5da2fec8b0dd858
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-01 17:42:01 +0100

    adding partial solution to persisting unmanaged bashrc additions

--- src/templates/bashrc-build.sh
+++ src/templates/bashrc-build.sh
@@ -1,21 +1,36 @@
 #! /bin/bash
 
-function buildSymlinks() {
+function persistUnmanagedAdditions() {
+  # search in the backup folder's .bashrc for lines after the ############# END BASH.D INCLUDE #############
+  # if any exist then add them to the bottom of the persistent bashrc
+  # put a backup in the backup folder and make the restore function put it back in place if anything goes wrong
+  return
+}
+
+
+function sourceBashrc() {
   local bash_perm_backup=".bashrc.orig_DO-NOT-DELETE"
 
   if [ ! -L $HOME/.bashrc ] && [ -f $HOME/.bashrc ]; then
     mv $HOME/.bashrc $HOME/$bash_perm_backup
+
+    echo "############# INCLUDED BY THE DOTFILES MULTIPLEXER     #############" >> $HOME/$bash_perm_backup
+    echo "#############   UNMANAGED ADDITIONS                    #############" >> $HOME/$bash_perm_backup
+    echo "# additions made by other applications and synchronised here by dotfiles-multiplexer" >> $HOME/$bash_perm_backup
+    echo "# persistent modifications to these values must be made in $HOME/$bash_perm_backup" >> $HOME/$bash_perm_backup
   fi
 
   if [ ! -f $HOME/$bash_perm_backup ]; then
-    echo "No source .bashrc file could be found. Is it possible that you or some other process has renamed it?"
+    echo "No source .bashrc file could be found. Is it possible that you, me or some other process has renamed it?"
     exit 1
   fi
 
   cp $HOME/$bash_perm_backup $build/.bashrc
-  
-  echo "############# INCLUDED BY THE DOTFILES MULTIPLEXER #############" >> $build/.bashrc
-  echo "############# BASH.D INCLUDE #############" >> $build/.bashrc
+}
+
+function buildSymlinks() {
+  echo "#############   END UNMANAGED ADDITIONS                #############" >> $build/.bashrc
+  echo "#############   BASH.D INCLUDE                         #############" >> $build/.bashrc
   echo "# runs a collection of bash scripts. this is analogeous to the " >> $build/.bashrc
   echo "# /etc/profile.d folder but for interactive bash shells rather " >> $build/.bashrc
   echo "# than login shells" >> $build/.bashrc
@@ -27,11 +42,19 @@ function buildSymlinks() {
   echo "  done" >> $build/.bashrc
   echo "  unset i" >> $build/.bashrc
   echo "fi" >> $build/.bashrc
+  echo "#############   END BASH.D INCLUDE                     #############" >> $build/.bashrc
+  echo "############# END INCLUDED BY THE DOTFILES MULTIPLEXER #############" >> $build/.bashrc
+}
+
+function buildBashrc() {
+  persistUnmanagedAdditions
+  sourceBashrc
+  buildSymlinks
 }
 
 if [ ! $multiplexer ]; then
-  echo "This script should only be run by the dotfile-multiplexer"
+  echo "This script should only be run by the dotfiles-multiplexer"
   exit 1
 fi
 
-buildSymlinks $@
+buildBashrc $@
