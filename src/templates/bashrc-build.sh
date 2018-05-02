097afad8c78b4df1ef80066d313f2a3667710df1
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-02 01:21:44 +0100

    adding housekeeping revert of persistance inclusion

--- src/templates/bashrc-build.sh
+++ src/templates/bashrc-build.sh
@@ -1,36 +1,38 @@
 #! /bin/bash
 
 function persistUnmanagedAdditions() {
-  # search in the backup folder's .bashrc for lines after the ############# END BASH.D INCLUDE #############
-  # if any exist then add them to the bottom of the persistent bashrc
-  # put a backup in the backup folder and make the restore function put it back in place if anything goes wrong
-  return
+  local additions=$(sed -n '/\#\#\#\#\#\#\#\#\#\#\#\#\# END INCLUDED BY THE DOTFILES MULTIPLEXER \#\#\#\#\#\#\#\#\#\#\#\#\#/ { s///; :a; n; p; ba; }' $build_backup/.bashrc)
+  if [ ! -z "$additions" ]; then
+    cp $HOME/$bashrc_persist_file_name $build_backup/$bashrc_persist_file_name
+    printf "\n$additions" >> $HOME/$bashrc_persist_file_name
+
+    sourceBashrc
+  fi
 }
 
-
 function sourceBashrc() {
-  local bash_perm_backup=".bashrc.orig_DO-NOT-DELETE"
-
   if [ ! -L $HOME/.bashrc ] && [ -f $HOME/.bashrc ]; then
-    mv $HOME/.bashrc $HOME/$bash_perm_backup
+    mv $HOME/.bashrc $HOME/$bashrc_persist_file_name
 
-    echo "############# INCLUDED BY THE DOTFILES MULTIPLEXER     #############" >> $HOME/$bash_perm_backup
-    echo "#############   UNMANAGED ADDITIONS                    #############" >> $HOME/$bash_perm_backup
-    echo "# additions made by other applications and synchronised here by dotfiles-multiplexer" >> $HOME/$bash_perm_backup
-    echo "# persistent modifications to these values must be made in $HOME/$bash_perm_backup" >> $HOME/$bash_perm_backup
+    echo "" >> $HOME/$bashrc_persist_file_name
+    echo "############# INCLUDED BY THE DOTFILES MULTIPLEXER     #############" >> $HOME/$bashrc_persist_file_name
+    echo "############# UNMANAGED ADDITIONS                      #############" >> $HOME/$bashrc_persist_file_name
+    echo "# additions made by other applications and synchronised here by dotfiles-multiplexer" >> $HOME/$bashrc_persist_file_name
+    echo "# persistent modifications to these values must be made in $HOME/$bashrc_persist_file_name" >> $HOME/$bashrc_persist_file_name
   fi
 
-  if [ ! -f $HOME/$bash_perm_backup ]; then
+  if [ ! -f $HOME/$bashrc_persist_file_name ]; then
     echo "No source .bashrc file could be found. Is it possible that you, me or some other process has renamed it?"
     exit 1
   fi
 
-  cp $HOME/$bash_perm_backup $build/.bashrc
+  cp $HOME/$bashrc_persist_file_name $build/.bashrc
 }
 
 function buildSymlinks() {
-  echo "#############   END UNMANAGED ADDITIONS                #############" >> $build/.bashrc
-  echo "#############   BASH.D INCLUDE                         #############" >> $build/.bashrc
+  echo "" >> $build/.bashrc
+  echo "############# END UNMANAGED ADDITIONS                  #############" >> $build/.bashrc
+  echo "############# BASH.D INCLUDE                           #############" >> $build/.bashrc
   echo "# runs a collection of bash scripts. this is analogeous to the " >> $build/.bashrc
   echo "# /etc/profile.d folder but for interactive bash shells rather " >> $build/.bashrc
   echo "# than login shells" >> $build/.bashrc
@@ -42,13 +44,13 @@ function buildSymlinks() {
   echo "  done" >> $build/.bashrc
   echo "  unset i" >> $build/.bashrc
   echo "fi" >> $build/.bashrc
-  echo "#############   END BASH.D INCLUDE                     #############" >> $build/.bashrc
+  echo "############# END BASH.D INCLUDE                       #############" >> $build/.bashrc
   echo "############# END INCLUDED BY THE DOTFILES MULTIPLEXER #############" >> $build/.bashrc
 }
 
 function buildBashrc() {
-  persistUnmanagedAdditions
   sourceBashrc
+  persistUnmanagedAdditions
   buildSymlinks
 }
 
